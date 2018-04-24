84a63ca3fc56933c970635a9faa81b6db6875233
Francisco Lucas Sens <francisco.lucas.sens@gmail.com>
2018-04-24 14:24:41 -0300

    Inicialização do repositório*

---
+++ ListaWhile/ListaWhile/Exemplo04.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExemploWhile
+{
+    class Exemplo04
+    {
+        public Exemplo04()
+        {
+            // TODO: corrigir o problema da média
+
+            int soma = 0, maiorNumero = int.MinValue, menorNumero = int.MaxValue;
+            Console.WriteLine(maiorNumero);
+            string historico = "";
+
+            Console.WriteLine("Digite um número, caso queira sair digite 8001");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            int quantidade = 0, quantidadePositivo = 0, quantidadeNegativo = 0, quantidadeNeutro = 0;
+            while (numero != 8001)
+            {
+                soma = soma + numero;
+                quantidade = quantidade + 1;
+
+                historico = historico + numero + ", ";
+
+                if (numero > maiorNumero)
+                {
+                    maiorNumero = numero;
+                }
+
+                if (numero < menorNumero)
+                {
+                    menorNumero = numero;
+                }
+
+                if (numero > 0)
+                {
+                    quantidadePositivo = quantidadePositivo + 1;
+                }
+                else if (numero != 0)
+                {
+                    quantidadeNegativo = quantidadeNegativo + 1;
+                }
+                else
+                {
+                    quantidadeNeutro = quantidadeNeutro + 1;
+                }
+
+                Console.WriteLine("Digite um número, para sair digite 8001");
+                numero = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Clear();
+            double media = soma / quantidade;
+            Console.WriteLine("Soma: " + soma);
+            Console.WriteLine("Média: " + media);
+            Console.WriteLine("Menor número: " + menorNumero);
+            Console.WriteLine("Maior número: " + maiorNumero);
+            Console.WriteLine("Quantidade Positivos: " + quantidadePositivo);
+            Console.WriteLine("Quantidade Negativos: " + quantidadeNegativo);
+            Console.WriteLine("Quantidade Neutros: " + quantidadeNeutro);
+            Console.WriteLine("Histórico: " + historico);
+        }
+    }
+}
---
+++ ListaWhile/ListaWhile/Principal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploWhile
+{
+    class Principal
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+            bool validarNumero = true;
+            while(validarNumero == true)
+            {
+                try
+                {
+                    Console.Write("Número: ");
+                    numero = Convert.ToInt32(Console.ReadLine());
+                    if (numero > 0)
+                    {
+                         validarNumero = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Número somente > 0");
+                    }
+                }
+                catch (Exception e)
+                {
+            Console.WriteLine("Número deveria conter somente números");
+                }
+            }
+        }
+    }
+}
